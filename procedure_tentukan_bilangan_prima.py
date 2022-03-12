e0333658fd7d8b484bd1edf6eb38a4287299ecaf
nahdluddin <93822283+NAHDLUDDIN@users.noreply.github.com>
2022-03-12 22:26:29 +0700

    Add files via upload

---
+++ procedure_tentukan_bilangan_prima.py
@@ -0,0 +1,15 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def prima(angka):
+    if angka > 1 :
+        for i in range (2, angka):
+            if angka%i==0:
+                print (angka, "adalah bukan bilangan prima")
+                break
+        else:
+            print (angka, "adalah bilangan prima")
+    else:
+        print (angka, "adalah bukan bilangan prima")
+angka = int(input('masukkan angka: '))
+prima(angka)
