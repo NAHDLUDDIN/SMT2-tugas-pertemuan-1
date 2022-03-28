888f0a3ac688e68e373339a50aa34472ed6c7fb1
nahdluddin <93822283+NAHDLUDDIN@users.noreply.github.com>
2022-03-29 05:28:26 +0700

    Update cek_bilangan_prima.py

--- cek_bilangan_prima.py
+++ cek_bilangan_prima.py
@@ -1,12 +1,33 @@
-#pengecekan bilangan prima atau bukan
+#pengecekan bilangan prima pada range tertentu dan hasilnya dimasukkan ke dalam stack
 angka=int(input("masukkan angka: "))
+stack = []
 if angka > 1:
-    for i in range (2, angka):
-        if angka % i == 0:
-            print (angka, "adalah bukan bilangan prima")
-            break
-    else:
-        print (angka, "adalah bilangan prima")
-else:
-    print (angka, "adalah bukan bilangan prima")
+#karena index dimulai dari angka 0 maka angka harus ditambahkan 1 pada rangenya
+    for i in range (2, angka +1):
+        for j in range (2, i):
+            if i % j == 0:
+                break
+        else:
+#memasukkan hasil i (bilPrima) ke dalam stack 
+            stack.append(i)
+#stack  pada kondisi awal            
+print (stack)
+#(LIFO) mengeluarkan item pada posisi paling atas yang terakhir masuk
+stack.pop()
+print (stack)
+#mengeluarkan item pada index tertentu contoh pop index ke 2, maka angka 5 pada bilangan prima 
+#akan di keluarkan dengan asumsi banyaknya indek adalah 2 atau lebih
+stack.pop(2)
+print (stack)
+
+            
+            
+            
+            
+           
+           
+            
+          
+          
+            
     
